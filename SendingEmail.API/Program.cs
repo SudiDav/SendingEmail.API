708313cda58f756ba4b4023bb42700607ac16100
Sudi <sudisimbadav@gmail.com>
2023-03-06 15:23:27 +0200

    changed configuration credentialas

--- SendingEmail.API/Program.cs
+++ SendingEmail.API/Program.cs
@@ -4,17 +4,13 @@ global using SendingEmail.API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IEmailService, EmailService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -27,7 +23,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapPost("email", (EmailDto request, IEmailService emailService) =>
+app.MapPost("/email", (EmailDto request, IEmailService emailService) =>
 {
     emailService.SendEmail(request);
 
